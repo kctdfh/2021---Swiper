7759adf6230d0d42aabfe26994dfa7dfe45a8bcf
Khashayar Toodehfallah <khashayar@toodehfallah.com>
2022-01-15 19:00:06 -0500

    virtual slides / video slide / link / intro

---
+++ js/slides.js
@@ -0,0 +1,34 @@
+var cometSlides = [
+  {
+    id: "comet-1",
+    type: "image",
+    src: "/img/comet-1.png",
+  },
+  {
+    id: "comet-2",
+    type: "image",
+    src: "/img/comet-2.png",
+  },
+  {
+    id: "comet-3",
+    type: "video",
+    src: "/img/comet-3.mp4",
+  },
+  {
+    id: "comet-4",
+    type: "image",
+    src: "/img/comet-4.png",
+  },
+  {
+    id: "comet-5",
+    type: "image",
+    src: "/img/comet-5.png",
+  },
+  {
+    id: "comet-6",
+    type: "image",
+    src: "/img/comet-6.png",
+    link: "www.google.com",
+    linkText: "Check out Google",
+  },
+];
